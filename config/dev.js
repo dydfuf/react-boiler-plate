ad76cf1428e96d28131c7c5a03962f42d53e03f3
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-06 18:30:36 +0900

    #commit

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dydfuf:dydfuf1@boilerplate.xlkqa.mongodb.net/react-chat-app?retryWrites=true&w=majority',
+}
--- index.js
+++ index.js
@@ -2,14 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyPaerser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded
+app.use(bodyPaerser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyPaerser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dydfuf:dydfuf1@boilerplate.xlkqa.mongodb.net/react-chat-app?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
  
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 새해복 많이 받으세요!')
+})
+
+app.post('/register', (req, res) => {
+    
+    //회원 가입 할때 필요한 정보들을 클라이언트에서 가져오면
+    //그것들을 데이터 베이스에 넣어 준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 app.listen(port, () => {
