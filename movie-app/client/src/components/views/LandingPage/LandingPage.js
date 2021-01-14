30240f880124f1f54d48c4cd2620d763ba4e2617
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-15 01:20:32 +0900

    Landing Page

--- movie-app/client/src/components/views/LandingPage/LandingPage.js
+++ movie-app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,37 +1,53 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { withRouter } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config'
+import MainImage from './Sections/MainImage'
 
 function LandingPage(props) {
 
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response.data))
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            
+            setMovies([response.result])
+            setMainMovieImage(response.results[0])
+        })
+
     }, [])
 
-    const onClickHandler = () =>{
-        axios.get('/api/users/logout')
-        .then(response => {
-            if(response.data.success){
-                props.history.push('/login')
-            } else {
-                alert('로그아웃 실패!')
-            }
-        })
-    }
 
     return (
-        <div style={{
-            display:'flex', justifyContent: 'center', alignItems: 'center',
-            width: '100%', height: '100vh'
-        }}>
-            <h2>시작 페이지</h2>
+        <div style={{width: '100%', margin: '0'}}>
 
-            <button onClick={onClickHandler}>
-                로그아웃
-            </button>
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}
+                 />
+            }
+
+            <div style={{width: '85%', margin: '1rem auto'}}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Moive Grid Cards */}
+
+            </div>
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button> Load More </button>
+            </div>
+
         </div>
     )
 }
 
-export default withRouter(LandingPage)
+export default LandingPage
