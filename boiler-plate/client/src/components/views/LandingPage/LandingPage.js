f5394443d4444f6f4f02dcf11ecba8dda0c22cb8
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-02-05 03:56:34 +0900

    boiler-plate change commit

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function LandingPage(props) {
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/profile")
+        .then(response => console.log(response))
+    }, [])
+
     return (
         <div style={{
             display:'flex', justifyContent: 'center', alignItems: 'center',
