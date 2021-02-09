f5f952ec7eb695374bfaf34331b4a7aa69dfe1b7
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-02-09 20:29:47 +0900

    캐러셀 슬라이드

--- maphacker/client/src/components/views/LandingPage/Sections/Card.js
+++ maphacker/client/src/components/views/LandingPage/Sections/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Col } from 'antd'
+import styled from 'styled-components'
 
-function Card(props) {
+const IMG = styled.img`
+  width: 50%;
+  height: 35vh;
+`;
+
+function Card({ img }) {
     return (
-        <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.cardName} />
-            </div>
-        </Col>
+        <IMG src={img} />
     )
 }
 
--- maphacker/client/src/components/views/LandingPage/Sections/CardList.js
+++ maphacker/client/src/components/views/LandingPage/Sections/CardList.js
@@ -1,32 +1,67 @@
-import React from 'react'
-import { Col } from 'antd'
+import React, { useEffect, useState, useRef } from 'react'
 import Card from './Card'
+import img1 from './img.jpg'
+import styled from 'styled-components'
 import { Row } from 'antd'
 
 function CardList(props) {
-    return (
+    const Container = styled.div`
+  width: 100%;
+  overflow: hidden; // 선을 넘어간 이미지들은 보이지 않도록 처리합니다.
+`;
 
-        <div>
-            <h2> {props.title} </h2>
-            <Row gutter={[16, 16]}>
-                <Card
-                    image="https://media.vlpt.us/images/chy0428/post/78e91082-da19-428b-984a-c3e21d0103e0/%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
-                    cardName="test"
-                />
-                <Card
-                    image="https://media.vlpt.us/images/chy0428/post/78e91082-da19-428b-984a-c3e21d0103e0/%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
-                    cardName="test"
-                />
-                <Card
-                    image="https://media.vlpt.us/images/chy0428/post/78e91082-da19-428b-984a-c3e21d0103e0/%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
-                    cardName="test"
-                />
-                <Card
-                    image="https://media.vlpt.us/images/chy0428/post/78e91082-da19-428b-984a-c3e21d0103e0/%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.png"
-                    cardName="test"
-                />
-            </Row>
-        </div>
+    const Button = styled.button`
+  all: unset;
+  border: 1px solid coral;
+  padding: 0.5em 2em;
+  color: coral;
+  border-radius: 10px;
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    background-color: coral;
+    color: #fff;
+  }
+`;
+
+    const SliderContainer = styled.div`
+  width: 100%;
+  display: flex; //이미지들을 가로로 나열합니다.
+`;
+
+
+    const TOTAL_SLIDES = 2;
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideRef = useRef(null);
+    const nextSlide = () => {
+        if (currentSlide >= TOTAL_SLIDES) { // 더 이상 넘어갈 슬라이드가 없으면 슬라이드를 초기화합니다.
+            setCurrentSlide(0);
+        } else {
+            setCurrentSlide(currentSlide + 1);
+        }
+    };
+    const prevSlide = () => {
+        if (currentSlide === 0) {
+            setCurrentSlide(TOTAL_SLIDES);
+        } else {
+            setCurrentSlide(currentSlide - 1);
+        }
+    };
+    useEffect(() => {
+        slideRef.current.style.transition = "all 0.5s ease-in-out";
+        slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
+    }, [currentSlide]);
+    return (
+        <Container>
+            {currentSlide}
+            <SliderContainer ref={slideRef}>
+                <Card img={img1} />
+                <Card img={img1} />
+                <Card img={img1} />
+                
+            </SliderContainer>
+            <Button onClick={prevSlide}>Previous Slide</Button>
+            <Button onClick={nextSlide}>Next Slide</Button>
+        </Container>
     )
 }
 
